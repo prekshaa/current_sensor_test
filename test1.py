ff54afc97e2539c7da2186f1a88b135ff5479e73
prekshaa <46904413+prekshaa@users.noreply.github.com>
2019-04-23 14:15:07 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,32 +5,31 @@ import busio
 import adafruit_ina219
 from ina219 import INA219
 i2c = busio.I2C(board.SCL, board.SDA)
-
-    ina219 = adafruit_ina219.INA219(i2c_bus)
+ina219 = adafruit_ina219.INA219(i2c_bus)
      
-    print("ina219 test")
+print("ina219 test")
 
-  if (i2c.try_lock())
-	print("EXIT_FAILURE1");
+if (i2c.try_lock())
+  print("EXIT_FAILURE1");
 
   while not i2c.try_lock():
-	pass
+    pass
      
-    while True:
-        print("Bus Voltage:   {} V".format(ina219.bus_voltage))  ##b/w GND and V-    use 2 and 9
-        print("Shunt Voltage: {} mV".format(ina219.shunt_voltage / 1000))  ##b/w V- and V+
-        print("Load Voltage:  {} V".format(ina219.bus_voltage + ina219.shunt_voltage))  
-        print("Current:       {} mA".format(ina219.current))
-        print("")
+  while True:
+    print("Bus Voltage:   {} V".format(ina219.bus_voltage))  ##b/w GND and V-    use 2 and 9
+    print("Shunt Voltage: {} mV".format(ina219.shunt_voltage / 1000))  ##b/w V- and V+
+    print("Load Voltage:  {} V".format(ina219.bus_voltage + ina219.shunt_voltage))  
+    print("Current:       {} mA".format(ina219.current))
+    print("")
 
-	if(ina219.current >= 430)
-	  print("Warning: current exceeds limit!")
+  if(ina219.current >= 430)
+    print("Warning: current exceeds limit!")
 
-	except KeyboardInterrupt:
-		print(EXIT_FAILURE2)
+  except KeyboardInterrupt:
+    print(EXIT_FAILURE2)
 
-        time.sleep(1)
+time.sleep(1)
 
-	busio.I2C.unlock()
+busio.I2C.unlock()
 
 
